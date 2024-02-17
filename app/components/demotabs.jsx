f76fe45b20e4bab16ace7b9dcb79e6c3198dc6a8
Amine Filali Ansari <Aminefilaliansari@gmail.com>
2024-02-17 23:19:57 +0100

    🎨 improve structure

--- app/components/demotabs.jsx
+++ app/components/demotabs.jsx
@@ -1,100 +1,36 @@
-import { Tabs, InputNumber, Space, Select, Button } from "antd";
-import { SwapOutlined } from "@ant-design/icons";
+import { Tabs, Skeleton } from "antd";
+import Convertir from "./elements/convertir";
+import {
+  DollarOutlined,
+  LineChartOutlined,
+  SendOutlined,
+  BellOutlined,
+} from "@ant-design/icons";
 
 const items = [
   {
     key: "1",
     label: "Convertir",
-    children: (
-      <Space wrap>
-        <Space className="inline-block">
-          <div className="flex flex-col">
-            <label>Montant</label>
-            <InputNumber
-              style={{ width: 300,
-                height: 50}}
-              size= "large"
-              defaultValue={0}
-              formatter={(value) =>
-                `${value} $`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-              }
-              parser={(value) => value.replace(/\$\s?|(,*)/g, "")}
-            />
-          </div>
-        </Space>
-
-        <Space className="inline-block">
-          <div className="flex flex-col">
-            <label>De</label>
-            <Select
-              defaultValue="USD"
-              style={{
-                width: 300,
-                height: 50,
-              }}
-              options={[
-                {
-                  value: "USD",
-                  label: "🇺🇸 USD - Dollar",
-                },
-                {
-                  value: "EURO",
-                  label: "🇪🇺 EURO - Europe",
-                },
-                {
-                  value: "DIRHAM",
-                  label: "🇲🇦 DH - Dirham",
-                },
-              ]}
-            />
-          </div>
-        </Space>
-
-        <Button shape="circle" icon={<SwapOutlined />} />
-
-        <Space className="inline-block">
-          <div className="flex flex-col">
-            <label>vers</label>
-            <Select
-              defaultValue="EURO"
-              style={{
-                width: 300,
-                height: 50,
-              }}
-              options={[
-                {
-                  value: "USD",
-                  label: "🇺🇸 USD - Dollar",
-                },
-                {
-                  value: "EURO",
-                  label: "🇪🇺 EURO - Europe",
-                },
-                {
-                  value: "DIRHAM",
-                  label: "🇲🇦 DH - Dirham",
-                },
-              ]}
-            />
-          </div>
-        </Space>
-      </Space>
-    ),
+    icon: <DollarOutlined />,
+    children: <Convertir />,
   },
   {
     key: "2",
     label: "Envoyer",
-    children: "Content of Tab Pane 2",
+    icon: <SendOutlined />,
+    children: "Tab Envoyer",
   },
   {
     key: "3",
     label: "Graphique",
-    children: "Content of Tab Pane 3",
+    icon: <LineChartOutlined />,
+    children: "Tab Graphique",
   },
   {
     key: "4",
     label: "Alertes",
-    children: "Content of Tab Pane 3",
+    icon: <BellOutlined />,
+    children: "Tabs Alertes",
   },
 ];
 
@@ -102,9 +38,12 @@ export default function DemoTabs() {
   return (
     <>
       <Tabs
+        id="DemoTabs"
         defaultActiveKey="1"
         items={items}
-        className="bg-white p-5 rounded-md drop-shadow-md "
+        type="card"
+        size="middle"
+        className="bg-white rounded-md drop-shadow-md "
       />
     </>
   );
